65e8319aba18ee39c15d9fb6a9fc9335e22392d0
Raouf878 <cheribetraouf607@gmail.com>
2024-09-27 08:18:07 +0200

    MYSQL Bug fixed.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\todosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/zakimlih', [UserController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,5 +20,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+//homepage todo route
+Route::get('/',[todosController::class, 'index'])->name("todo.home");
 
+//create todo route
+Route::get('/create', function () {
+    return view('create');
+})->name("todo.create");
+
+//edit todo route
+Route::get('/edit/{id}',[todosController::class,'edit'])->name("todo.edit");
+
+//update todo route
+Route::post('/update', [todosController::class,'updateData'])->name("todo.updateData");
+
+//store todo route
+Route::post('/create', [todosController::class,'store'])->name("todo.store");
+
+//delete toto route
+Route::get('/delete/{id}', [todosController::class,'delete'])->name("todo.delete");
 require __DIR__.'/auth.php';
